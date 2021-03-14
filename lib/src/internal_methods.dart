838a114a0f6da5fff7b523ecff0e25147627f4e4
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-14 22:12:21 +0330

    feat(internal methods): added replaceMap function

--- lib/src/internal_methods.dart
+++ lib/src/internal_methods.dart
@@ -2,8 +2,16 @@
 /// code duplication
 
 /// Custom trim method with custom regexp that helps us to remove whitespaces
-String trim(String string) => string.replaceAll(RegExp(r'/^\s+|\s+$/g'), '');
+String trim(String string) => string.replaceAll(RegExp(r'^\s+|\s+$'), '');
 
 /// Arabic contextual forms in regexp
 final arabicContextualForms = RegExp(r'/[ي|ﻱ|ﻲ|ﻚ|ك|ﻚ|ﺔ|ﺓ|ة]/g');
 
+/// Takes two parameters - string and mapPattern - which are String and Map
+/// and create a [RegExp] pattern form keys of Map and if keys matched , replace
+/// matched string with corresponding value of key
+String replaceMap(String string, Map<String, String> mapPattern) {
+  // TODO: create test case and null checker for line:16
+  var regexp = RegExp(mapPattern.keys.join('|'), caseSensitive: false);
+  return string.replaceAllMapped(regexp, (match) => mapPattern[match.group(0)]);
+}
