d485fb9de36d5cf1713954f3f6efe4cd25bce8f8
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-14 13:07:33 +0330

    fix : addOrdinalSuffix in first condition

--- lib/src/add_ordinal_suffix.dart
+++ lib/src/add_ordinal_suffix.dart
@@ -1,12 +1,12 @@
 /// Adds Ordinal Suffixes to numbers
 /// for example :
 /// ```dart
-/// addOrdinalSuffix('سی'); // سی اٌم
+/// addOrdinalSuffix('سی'); // سی اُم
 /// addOrdinalSuffix('سی دو'); // سی دوم
 /// ```
 String addOrdinalSuffix(String number) {
   if (number.endsWith('ی')) {
-    return number + ' اٌم';
+    return number + ' اُم';
   }
   if (number.endsWith('سه')) {
     return number.substring(0, 3) + 'سوم';
