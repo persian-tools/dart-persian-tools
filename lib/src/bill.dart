e55d09466d0a84528f5a0064cd2e0d361a2eebef
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-15 01:38:26 +0330

    feat(bill) : declare billTypes variables

---
+++ lib/src/bill.dart
@@ -0,0 +1,17 @@
+/// Saves bill types to use it in our codes as ```List<String>```
+final List<String> billTypesList = [
+  'آب',
+  'برق',
+  'گاز',
+  'تلفن ثابت',
+  'تلفن همراه',
+  'عوارض شهرداری',
+  'سازمان مالیات',
+  'جرایم راهنمایی و رانندگی',
+];
+
+/// Saves bill types as a ```Map<int, String>```
+final Map<int, String> billTypesMap = Map.fromIterables(
+  List<int>.generate(9, (index) => index + 1),
+  billTypesList,
+);
