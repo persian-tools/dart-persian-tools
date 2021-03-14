3d5cbe976fd5f0fbc5363ba00abc59ad08e8a1bc
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-14 13:28:41 +0330

    fix(add ordinal suffix) : fix second condition in addOrdinalSuffix

--- lib/src/add_ordinal_suffix.dart
+++ lib/src/add_ordinal_suffix.dart
@@ -3,13 +3,14 @@
 /// ```dart
 /// addOrdinalSuffix('سی'); // سی اُم
 /// addOrdinalSuffix('سی دو'); // سی دوم
+/// addOrdinalSuffix('پنج'); // پنجم
 /// ```
 String addOrdinalSuffix(String number) {
   if (number.endsWith('ی')) {
     return number + ' اُم';
   }
   if (number.endsWith('سه')) {
-    return number.substring(0, 3) + 'سوم';
+    return number.substring(0, number.length - 2) + 'سوم';
   }
   return number + 'م';
 }
