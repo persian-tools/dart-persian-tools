08da1fc789c3910b1d53b8cf2d09aaeff7567d1a
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-14 12:46:41 +0330

    feat : added AddOrdinalSuffix extension on String

--- lib/src/add_ordinal_suffix.dart
+++ lib/src/add_ordinal_suffix.dart
@@ -1,8 +1,8 @@
 /// Adds Ordinal Suffixes to numbers
 /// for example :
 /// ```dart
-/// addOrdinalSuffix('سی') // سی اٌم
-/// addOrdinalSuffix('سی دو') // سی دوم
+/// addOrdinalSuffix('سی'); // سی اٌم
+/// addOrdinalSuffix('سی دو'); // سی دوم
 /// ```
 String addOrdinalSuffix(String number) {
   if (number.endsWith('ی')) {
@@ -12,4 +12,15 @@ String addOrdinalSuffix(String number) {
     return number.substring(0, 3) + 'سوم';
   }
   return number + 'م';
+}
+
+/// with AddOrdinalSuffix extension method you can use [addOrdinalSuffix] method
+/// as a getter on String object
+/// ```dart
+/// var number = 'سی سه';
+/// number.withOrdinalSuffix; // سی سوم
+/// ```
+extension AddOrdinalSuffix on String {
+  // TODO: add Persian language checker on this object
+  String get withOrdinalSuffix => addOrdinalSuffix(this);
 }
