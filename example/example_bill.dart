482af1ccd95b1b3f477b48f9a965eeb6706f96bf
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-16 18:33:27 +0330

    improvement(bill):updated example_bill.dart file

--- example/example_bill.dart
+++ example/example_bill.dart
@@ -2,11 +2,12 @@ import 'package:dart_persian_tools/dart_persian_tools.dart';
 
 void main() {
   var bill = Bill(billId: 9174639504124, paymentId: 12908197, currency: 'rial');
-  print(bill.barcode);
-  print(bill.billType);
-  print(bill.amount);
-  print(bill.isBillValid);
-  print(bill.isPaymentIdValid);
-  print(bill.isBillIdValid);
-  print(bill()); // returns all the above getter as a Map
+  print(bill.barcode); // 917463950412400012908197
+  print(bill.billType); // برق
+  print(bill.amount); // 129000
+  print(bill.isBillValid); // false
+  print(bill.isPaymentIdValid); // false
+  print(bill.isBillIdValid); // true
+  // returns all the above getter as a Map with same as getters
+  print(bill());
 }
