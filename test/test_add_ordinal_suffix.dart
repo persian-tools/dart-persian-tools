391041de97581b77541df9a1d33df13aa4ece645
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-16 15:29:29 +0330

    test(add ordinal suffix):update testcases

--- test/test_add_ordinal_suffix.dart
+++ test/test_add_ordinal_suffix.dart
@@ -3,41 +3,18 @@ import 'package:test/test.dart';
 
 void main() {
   group('test add_ordinal_suffix.dart', () {
-    group('test addOrdinalSuffix method', () {
-      test('method on پنجاه سه', () {
-        var number = 'پنجاه سه';
-        expect(addOrdinalSuffix(number), equals('پنجاه سوم'));
-      });
-      test('method on سی دو', () {
-        var number = 'سی دو';
-        expect(addOrdinalSuffix(number), equals('سی دوم'));
-      });
-      test('method on شصت', () {
-        var number = 'شصت';
-        expect(addOrdinalSuffix(number), equals('شصتم'));
-      });
-      test('method on سی', () {
-        var number = 'سی';
-        expect(addOrdinalSuffix(number), equals('سی اُم'));
-      });
+    test('test addOrdinalSuffix method', () {
+      expect(addOrdinalSuffix('چهل و سه'), equals('چهل و سوم'));
+      expect(addOrdinalSuffix('پنجاه و پنج'), equals('پنجاه و پنجم'));
+      expect(addOrdinalSuffix('صد و یک'), equals('صد و یکم'));
+      expect(addOrdinalSuffix('سی'), equals('سی اُم'));
     });
-    group('test AddOrdinalSuffix extension', () {
-      test('extension on پنجاه سه', () {
-        var number = 'پنجاه سه';
-        expect(number.withOrdinalSuffix, equals('پنجاه سوم'));
-      });
-      test('extension on سی دو', () {
-        var number = 'سی دو';
-        expect(number.withOrdinalSuffix, equals('سی دوم'));
-      });
-      test('extension on شصت', () {
-        var number = 'شصت';
-        expect(number.withOrdinalSuffix, equals('شصتم'));
-      });
-      test('extension on سی', () {
-        var number = 'سی';
-        expect(number.withOrdinalSuffix, equals('سی اُم'));
-      });
+    test('test AddOrdinalSuffix extension method', () {
+      expect('چهل و یک'.withOrdinalSuffix, equals('چهل و یکم'));
+      expect('شصت'.withOrdinalSuffix, equals('شصتم'));
+      expect('سی'.withOrdinalSuffix, equals('سی اُم'));
+      expect('هزار و صد و بیست و سه'.withOrdinalSuffix,
+          equals('هزار و صد و بیست و سوم'));
     });
   });
 }
