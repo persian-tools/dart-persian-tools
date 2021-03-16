42993ffb69a2e67ce44c045e9bac32db24435bb2
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-16 20:21:21 +0330

    docs(is persian):added isPersian method documents

--- lib/src/isPersian/methods.dart
+++ lib/src/isPersian/methods.dart
@@ -1,5 +1,8 @@
 import './fa_chars.dart';
 
+/// Check is the input string Persian or not
+/// returns true if the input is matched with Farsi char dataset [RegExp], if not
+/// returns false
 bool isPersian(String input, [bool isComplex = false, Pattern? trimPattern]) {
   trimPattern ??= RegExp('["' r"'-+()؟\s.]");
   var rawText = input.replaceAll(trimPattern, '');
