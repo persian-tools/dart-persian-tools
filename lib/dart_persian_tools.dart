372b00ea9926250c84bd6345cfcdcff7fd59ca64
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-16 15:38:21 +0330

    improvement(main lib):export bill.dart

--- lib/dart_persian_tools.dart
+++ lib/dart_persian_tools.dart
@@ -3,3 +3,6 @@ library dart_persian_tools;
 
 /// exports [addOrdinalSuffix] method and [AddOrdinalSuffix] extension
 export 'src/add_ordinal_suffix.dart';
+
+/// exports [Bill] class
+export 'src/bill.dart';
