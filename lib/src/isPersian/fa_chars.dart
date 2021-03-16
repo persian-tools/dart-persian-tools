770e6f18bf838effbd4519b527c89ba61dc65f38
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-16 18:59:31 +0330

    feat(is persian):added farsi char dataset

---
+++ lib/src/isPersian/fa_chars.dart
@@ -0,0 +1,20 @@
+/// Farsi Alphabets
+const faAlphabet = 'ابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی';
+
+/// Farsi Numbers
+const faNumber = '۰۱۲۳۴۵۶۷۸۹';
+
+/// Farsi Short Vowels
+const faShortVowels = 'َُِ';
+
+/// Others Farsi Letters
+const faOthers = '‌آاً';
+
+/// Farsi Letters Mixed with Arabic
+const faMixedWithArabic = 'ًٌٍَُِّْٰٔءك‌ةۀأإيـئؤ،';
+
+/// All Farsi Letters
+const faText = faAlphabet + faNumber + faShortVowels + faOthers;
+
+/// Letters that is common in Arabic and Farsi
+const faComplexText = faText + faMixedWithArabic;
