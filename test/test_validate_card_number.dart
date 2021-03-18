8b14f5ba725cb2dffa3e2de3e8d53a495fc89d63
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-18 19:38:07 +0330

    test(validate card number):create testcase

---
+++ test/test_validate_card_number.dart
@@ -0,0 +1,14 @@
+import 'package:dart_persian_tools/dart_persian_tools.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('test validate_card_number.dart', () {
+    test('test validateCardNumber', () {
+      expect(validateCardNumber('6037701689095443'), equals(true));
+      expect(validateCardNumber('6219861034529007'), equals(true));
+      expect(validateCardNumber('6219861034529008'), equals(false));
+      expect(validateCardNumber('621986103452900'), equals(false));
+      expect(validateCardNumber('0'), equals(false));
+    });
+  });
+}
