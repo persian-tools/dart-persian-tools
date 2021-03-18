f9637c3ece5a7038edad2b7d355b94d7773d2e09
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-18 19:16:38 +0330

    feat(validate card number):added validateCardNumber method

---
+++ lib/src/validate_card_number.dart
@@ -0,0 +1,13 @@
+/// Check validation of the given ATM card number
+bool validateCardNumber(String cardNumber) {
+  if (cardNumber.length < 16 ||
+      int.parse(cardNumber.substring(1, 11)) == 0 ||
+      int.parse(cardNumber.substring(10)) == 0) return false;
+  int sum = 0, even, subDigit;
+  for (var i = 0; i < 15; i++) {
+    even = i % 2 == 0 ? 2 : 1;
+    subDigit = int.parse(cardNumber[i]) * even;
+    sum += subDigit > 9 ? subDigit - 9 : subDigit;
+  }
+  return sum % 10 == 0;
+}
