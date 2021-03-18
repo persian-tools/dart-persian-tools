bd21d46722b88c1e8e91317ec7864cf799120828
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-18 19:35:57 +0330

    fix(validate card number):change number of iteration

--- lib/src/validate_card_number.dart
+++ lib/src/validate_card_number.dart
@@ -4,7 +4,7 @@ bool validateCardNumber(String cardNumber) {
       int.parse(cardNumber.substring(1, 11)) == 0 ||
       int.parse(cardNumber.substring(10)) == 0) return false;
   int sum = 0, even, subDigit;
-  for (var i = 0; i < 15; i++) {
+  for (var i = 0; i < 16; i++) {
     even = i % 2 == 0 ? 2 : 1;
     subDigit = int.parse(cardNumber[i]) * even;
     sum += subDigit > 9 ? subDigit - 9 : subDigit;
