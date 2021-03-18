0ba86578b9e42a33372bd521e6ac1a1b2e02b1e8
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-18 19:40:29 +0330

    feat(validate card number):added example for validateCardNumber

---
+++ example/example_validate_card_number.dart
@@ -0,0 +1,8 @@
+import 'package:dart_persian_tools/dart_persian_tools.dart';
+
+void main(){
+  print(validateCardNumber('6219861034529007')); // true
+  print(validateCardNumber('6219861034529007')); // true
+  print(validateCardNumber('0000000000000000')); // false
+  print(validateCardNumber('621986103452900')); // false
+}
