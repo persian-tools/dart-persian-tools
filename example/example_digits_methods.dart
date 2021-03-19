a66f463d4635be235ade25b38bdf1c6f76bb68e8
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-19 14:36:28 +0330

    feat(digits):added example for Digits/methods

---
+++ example/example_digits_methods.dart
@@ -0,0 +1,8 @@
+import 'package:dart_persian_tools/dart_persian_tools.dart';
+
+void main() {
+  print(convertArToFa('السلام علیکم 14۱۲۳6٤٥')); // السلام علیکم 14۱۲۳6۴۵
+  print(convertArToEn('Persian Tools : 123٥٦٧')); // Persian Tools : 123567
+  print(convertEnToFa('سلام این هارو فارسی کن : 22۲۳۴')); // سلام این هارو فارسی کن : ۲۲۲۳۴
+  print(convertFaToEn('سلام این هارو اینگلیسی کن : 22۲۳۴')); // سلام این هارو اینگلیسی کن : 22234
+}
