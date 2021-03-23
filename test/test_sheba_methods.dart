1f3ff1e6e8a93774a7befe48b39caa27da1a12f2
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-23 22:08:43 +0430

    test(sheba):added call method testcase

--- test/test_sheba_methods.dart
+++ test/test_sheba_methods.dart
@@ -9,5 +9,20 @@ void main() {
       expect(Sheba('IR012345678901234567890123').isValid, equals(false));
       expect(Sheba('IR01234567890123456789').isValid, equals(false));
     });
+    test('test call methods', () {
+      var sheba = Sheba('IR820540102680020817909002');
+      expect(
+          sheba(),
+          equals({
+            'nickname': 'parsian',
+            'name': 'Parsian Bank',
+            'persianName': 'بانک پارسیان',
+            'code': '054',
+            'accountNumberAvailable': true,
+            'accountNumber': '020817909002',
+            'formattedAccountNumber': '0020-0081790-002',
+          }));
+
+    });
   });
 }
