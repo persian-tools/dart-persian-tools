b1a239cfa366dc730a3d0b4f0f321a9f0e6835cc
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-03-26 11:08:16 +0430

    improvement(sheba):update example

--- example/example_sheba_methods.dart
+++ example/example_sheba_methods.dart
@@ -2,6 +2,24 @@ import 'package:dart_persian_tools/dart_persian_tools.dart';
 
 void main() {
   var sheba = Sheba('IR820540102680020817909002');
-  print(sheba()); // Returns all the information about bank from sheba
+  var bank = sheba(); // Returns nullable object of BankInformation
+  print(bank?.nickname); // nickname of bank
+  print(bank?.name); // complete name of bank
+  print(bank?.persianName); // complete persian name of bank
+  print(bank?.code); // code of bank
+  // true means with sheba package can take account number, false means package can't
+  print(bank?.accountNumberAvailable);
+  print(bank?.accountNumber); // account number in simple numbers
+  print(bank?.formattedAccountNumber); // account number with hyphen
+  print(bank?.process); // always is null just uses in call method in Sheba
   print(sheba.isValid); // true
+  var b = BankInformation(
+      nickname: 'parsian',
+      name: 'Parsian Bank',
+      persianName: 'بانک پارسیان',
+      code: '054',
+      accountNumberAvailable: true,
+      accountNumber: '020817909002',
+      formattedAccountNumber: '0020-0081790-002');
+  print(bank == b);
 }
