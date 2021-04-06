882662e8a65121084c176b349aebf02485f32fb8
Payam Zahedi <pzahedi1995@gmail.com>
2021-04-06 12:41:44 +0430

    improvement(PhoneNumber): update README.md

--- lib/src/core/phone_number/phone_number.dart
+++ lib/src/core/phone_number/phone_number.dart
@@ -2,6 +2,11 @@ import 'package:dart_persian_tools/src/constants/phone_number/constants.dart';
 
 import 'models.dart';
 
+/// returns the phone number prefix
+/// use it like:
+/// ```dart
+///   getPhonePrefix('09022002580'); // 902
+/// ```
 String getPhonePrefix(String phoneNumber) {
   final matches = mobileRegex.allMatches(phoneNumber);
 
@@ -12,6 +17,25 @@ String getPhonePrefix(String phoneNumber) {
   return '';
 }
 
+
+/// find information such as province, type and model of [phoneNumber]
+/// this function used the [getPhonePrefix] to find out the [phoneNumber] prefix number and
+/// used the [phoneNumberValidator] function to validate the [phoneNumber].
+/// support the following phone number formats
+/// 1. 00989123456789
+/// 2. +989123456789
+/// 3. 989123456789
+/// 4. 09123456789
+/// 5. 9123456789
+///
+/// use it like:
+/// ```dart
+/// final operatorDetail = getPhoneNumberDetail(phoneNumber);
+/// print(operatorDetail?.name); // ایرانسل
+/// print(operatorDetail?.provinces); // []
+/// print(operatorDetail?.base); // کشوری
+/// print(operatorDetail?.type); // SimCartType.both
+/// ```
 OperatorDetail? getPhoneNumberDetail(String phoneNumber) {
   if (phoneNumberValidator(phoneNumber)) {
     final prefix = getPhonePrefix(phoneNumber);
@@ -20,6 +44,8 @@ OperatorDetail? getPhoneNumberDetail(String phoneNumber) {
   }
 }
 
+/// validate [phoneNumber] and returns [true] if the [phoneNumber] parameter matched with pattern
+/// otherwise returns false.
 bool phoneNumberValidator(String phoneNumber) {
   return mobileRegex.hasMatch(phoneNumber) &&
       prefixes.contains(getPhonePrefix(phoneNumber));
