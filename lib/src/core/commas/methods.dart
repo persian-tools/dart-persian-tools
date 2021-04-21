7003b5fa5adf2143091c02bfb0f09c88368db82f
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-04-21 14:21:58 +0430

    feat(commas):added methods addCommas, removeCommas

--- lib/src/core/commas/methods.dart
+++ lib/src/core/commas/methods.dart
@@ -1,13 +1,15 @@
 import '../digits/methods.dart';
 import '../is_persian/methods.dart';
 
+/// Takes an integer or string and add commas as separator
+/// NOTE: it converts Fa digits to En digits
 String addCommas(Object number) {
-  if (number.runtimeType is! int || number.runtimeType is! String) {
+  if (number is! int && number is! String && number is! double) {
     throw Exception('Type should be String or int');
   }
   final numberStr = number.runtimeType is String
-      ? number as String
-      : (number as int).toString();
+      ? number as String //
+      : number.toString();
   final enNumberStr =
       isPersian(numberStr) ? convertFaToEn(numberStr) : numberStr;
   final decimalNumber = enNumberStr.split('.');
@@ -22,4 +24,12 @@ String addCommas(Object number) {
     decimalPart = '';
   }
   return integerPart + decimalPart;
+}
+
+///Takes a string and it should be includes integer then remove all the commas
+num removeCommas(String number) {
+  if (number.contains(',')) {
+    number = number.replaceAll(RegExp(r',\s?'), '');
+  }
+  return num.parse(number);
 }
