e7eb653e5da7a8537bbf50db8536667280796f4b
Payam Zahedi <pzahedi1995@gmail.com>
2021-05-27 22:44:06 +0430

    test(WordsToNumber): added test and function body

---
+++ lib/src/core/words_to_number/words_to_number.dart
@@ -0,0 +1,25 @@
+enum DigitLocale {
+  en,
+  fa,
+  ar,
+}
+
+int compute(List<String> tokens) {
+  return 0;
+}
+
+List<String> tokenize(String words) {
+  return [];
+}
+
+int wordsToNumber(String words) {
+  return 0;
+}
+
+String wordsToNumberString(
+  String words, {
+  DigitLocale digits = DigitLocale.en,
+  bool addComma = false,
+}) {
+  return '0';
+}
