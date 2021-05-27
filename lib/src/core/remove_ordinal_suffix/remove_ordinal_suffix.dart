53271c060a24013b1608e51f5ce2fc753c663116
Payam Zahedi <pzahedi1995@gmail.com>
2021-05-28 00:19:37 +0430

    feat(RemoveOrdinalSuffix): added function body

--- lib/src/core/remove_ordinal_suffix/remove_ordinal_suffix.dart
+++ lib/src/core/remove_ordinal_suffix/remove_ordinal_suffix.dart
@@ -1,11 +1,22 @@
 /// removes Ordinal Suffixes to numbers
 /// for example :
 /// ```dart
-/// removeOrdinalSuffix("سه هزارم") // سث هزار
-/// removeOrdinalSuffix("سه هزارمین") // سه هزار
+/// removeOrdinalSuffix('سه هزارم') // سث هزار
+/// removeOrdinalSuffix('سه هزارمین') // سه هزار
 /// ```
-String removeOrdinalSuffix(String number) {
-  return number;
+String removeOrdinalSuffix(String word) {
+  if (word.isEmpty) return word;
+
+  word = word
+      .replaceAll(RegExp('مین\$', caseSensitive: false), '')
+      .replaceAll(RegExp('(ام| اُم)\$', caseSensitive: false), '');
+  if (word.endsWith('سوم')) {
+    word = word.substring(0, word.length-3) + 'سه';
+  } else if (word.endsWith('م')) {
+    word = word.substring(0, word.length-1);
+  }
+
+  return word;
 }
 
 /// with RemoveOrdinalSuffix extension method you can use [removeOrdinalSuffix] method
