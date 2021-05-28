f3fcfaf3114d654eeeb4250a85746a5e44100c49
makhosravi <peter_stark95@yahoo.com>
2021-05-28 19:16:22 +0430

    test(NumberToWords): added test and function body

---
+++ lib/src/core/number_to_words/number_to_words.dart
@@ -0,0 +1,11 @@
+String numToWord(int number) {
+  return 'صفر';
+}
+
+String numberToWordsByString (String number, {bool ordinal = false}) {
+  return 'صفر';
+}
+
+String numberToWordsByInt (int number, {bool ordinal = false}) {
+  return 'صفر';
+}
