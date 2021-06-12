4fd9df1612384a80635853d2c8a9e33a64fd9c01
Ehsan Aramide <ehsanaramidegithub@gmail.com>
2021-06-12 14:00:27 +0430

    docs(GetPlaceByNationalId): added doc comments

--- lib/src/core/get_place_by_national_id/get_place_by_national_id.dart
+++ lib/src/core/get_place_by_national_id/get_place_by_national_id.dart
@@ -1,7 +1,8 @@
 import 'package:persian_tools/src/constants/get_place_by_national_id/constants.dart';
 import 'package:persian_tools/src/core/get_place_by_national_id/models.dart';
-import 'package:persian_tools/persian_tools.dart';
+import 'package:persian_tools/src/core/national_id/national_id.dart';
 
+/// Gives the place information based on the given String as national id
 NationalIdPlace? getPlaceByIranNationalId(String nationalId) {
   if (!nationalId.isIranianNationalId) return null;
 
@@ -26,6 +27,8 @@ NationalIdPlace? getPlaceByIranNationalId(String nationalId) {
   return null;
 }
 
+/// [String] Extension wrapper to getting place information from String object
+/// as national id
 extension PlaceByNationalId on String {
   NationalIdPlace? get getPlaceNationalId => getPlaceByIranNationalId(this);
 }
